b56a48ad35de21c918eab450d4779c61d52e0797
Maximilian Stenlund <stenlundmaximilian@gmail.com>
2023-07-16 16:53:26 +0200

    register post with users

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,43 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+
 const router = express.Router()
 
 router.get('/',(req,res)=>{
     res.render('register/index',{headerLinks:[]})
 })
 
-module.exports = router
+module.exports = (users) => {
+    router.post('/', async (req, res) => {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+          id: Date.now().toString(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+        });
+        res.redirect('/login');
+      } catch {
+        res.redirect('/register');
+      }
+    });
+    return router;
+  };
+  
+//router.post('/',async (req,res)=>{
+//    try{
+//        const hashedPassword = await bcrypt.hash(req.body.password,10)
+//        users.push({
+//            id: Date.now().toString(),
+//            name: req.body.name,
+//            email: req.body.email,
+//            password: hashedPassword
+//        })
+//        res.redirect('/login')
+//    } catch{
+//        res.redirect('/register')
+//    }
+//})
+
+//module.exports = router
