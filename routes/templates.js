cda9495dfb630a19d87bed79c83767e5f9613e0d
Maximilian Stenlund <stenlundmaximilian@gmail.com>
2023-07-19 18:46:25 +0200

    working dropdown menu

--- routes/templates.js
+++ routes/templates.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Character = require('../models/character')
 
 const checkAuthenticated = require('../models/checkAuthentication').checkAuthenticated
 
-router.get('/',checkAuthenticated,(req,res)=>{
-    res.render('templates/index',{
-        headerLinks:[]
-    })
+router.get('/',checkAuthenticated,async(req,res)=>{
+    try{
+        const characters = await Character.find({createdBy:req.user._id})
+        res.render('templates/index',{
+            headerLinks:[],
+            characters:characters
+        })
+    } catch(err){
+        console.error(err)
+    }
 })
 
 module.exports = router
