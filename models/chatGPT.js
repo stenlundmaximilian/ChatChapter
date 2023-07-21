ea4ba09db834a433d3b107d004998428dee16c74
Maximilian Stenlund <stenlundmaximilian@gmail.com>
2023-07-21 15:52:54 +0200

    working chatGPT API

---
+++ models/chatGPT.js
@@ -0,0 +1,26 @@
+async function fetchData(API_KEY,character1,character2){
+    const response = await fetch("https://api.openai.com/v1/chat/completions",{
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            temperature: 0.6,
+            messages: [
+                {"role":"system", "content":"Generate short stories with the given information"},
+                {
+                    "role":"user",
+                    "content":`title:pirate adventure,\n
+                                character1:{name:${character1.name}, hobby:${character1.hobby}, trait:${character1.trait}},\n
+                                character2:{name:${character2.name}, hobby:${character2.hobby}, trait:${character2.trait}}`
+                }
+            ]
+        })
+    })
+    const data = await response.json();
+    return data.choices[0].message.content;
+}
+
+module.exports = fetchData
--- routes/templates.js
+++ routes/templates.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Character = require('../models/character')
 
 const checkAuthenticated = require('../models/checkAuthentication').checkAuthenticated
+const fetchData = require('../models/chatGPT')
 
 router.get('/',checkAuthenticated,async(req,res)=>{
     try{
@@ -13,6 +14,46 @@ router.get('/',checkAuthenticated,async(req,res)=>{
         })
     } catch(err){
         console.error(err)
+    }
+})
+
+router.get('/generate',checkAuthenticated,async (req,res)=>{
+    try{
+        const maxLength = 80;
+        const _id1 = req.query.character1
+        const _id2 = req.query.character2
+        const character1 = await Character.findById(_id1)
+        const character2 = await Character.findById(_id2)
+        const message = await fetchData(process.env.API_KEY,character1,character2)
+        const regex = new RegExp(`.{1,${maxLength}}`, 'g');
+        const formatedMessage = message.match(regex).join('\n');
+        //const message = `
+//===============================
+//   Welcome to Console Page   
+//===============================
+//
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec tortor quis erat semper aliquam.
+//Suspendisse luctus quam vel nunc sagittis vestibulum. Nullam auctor euismod fringilla. Sed ac nulla ac massa congue suscipit.
+//
+//Phasellus et mi vitae nulla rhoncus auctor. Aenean fermentum laoreet eros ac efficitur. Nullam ut aliquam dui, at blandit arcu.
+//Quisque ultrices ipsum at odio tristique fringilla. Donec bibendum, tortor et tincidunt rutrum, turpis tellus bibendum augue, id pharetra turpis ligula non dolor.
+//
+//===============================
+//          Contact Us          
+//===============================
+//Email: contact@example.com
+//Phone: +1 (555) 123-4567
+//
+//Thank you for visiting!
+//`    
+        console.log(message)
+        res.render('templates/generate',{
+            headerLinks:[],
+            message:formatedMessage
+
+        })
+    } catch(err){
+        console.error(err);
     }
 })
 
